ad87cf3c831aa844e24acd900f7aea8f3aaf04ad
scarf <greenscarf005@gmail.com>
2025-03-07 22:38:50 +0900

    feat: CLI

--- JJKlang-scala/main.scala
+++ JJKlang-scala/main.scala
@@ -1,3 +1,19 @@
+import scala.io.Source.*
 
-@main def main() =
-  println("Hello, World!")
+import parser.*
+import interpreter.*
+
+def run(path: String)(using ctx: InterpreterContext) = for
+  file = fromFile(path).mkString
+  interpreter = Interpreter()(using ctx)
+  code <- program.parse(file)
+yield interpreter.eval(code).`return`
+
+@main def main(args: String*) = args.toList.partition(_.startsWith("--")) match
+  case (flags, List(file)) =>
+    given ctx: InterpreterContext =
+      if flags.toSet("--no-sleep") then SleeplessContext else IoContext
+
+    run(file)(using ctx).fold(err => { println(err); 1 }, identity)
+
+  case _ => println("Usage: <program> -- <file> [--no-sleep]"); 1
